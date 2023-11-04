d4c74c46b1ab89311b7e80623934b95adecec5be
stitou-med <mstitou044@gmail.com>
2023-11-04 17:52:36 +0100

    11/4/23

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,11 +8,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-    char *p = s;
-    while (n-->0)
-    {
-        *p = b;
-        p++;
-    }
-    return s;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
+	return (s);
 }
+
